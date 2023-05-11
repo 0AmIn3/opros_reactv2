ec1738dc9db7973e151116dc44cbc2d48b649df9
0Amin3 <aminfedya2888@gmail.com>
2023-05-11 19:13:19 +0500

    defeferfwfw

--- src/features/goods/thunk.js
+++ src/features/goods/thunk.js
@@ -48,18 +48,18 @@ import { createAsyncThunk } from "@reduxjs/toolkit"
 // })
 
 export const getUsersAPI = createAsyncThunk('goods/getUsersAPI', async () => {
-    const res = await axios.get('http://localhost:3001/users')
+    const res = await axios.get('http://localhost:7777/users')
     
     return res.data
 })
 
 export const postUsersAPI = createAsyncThunk('goods/postUsersAPI', async (data) => {
-    const res = await axios.post('http://localhost:3001/users', data)
+    const res = await axios.post('http://localhost:7777/users', data)
     
     return res.data
 })
 export const pathUsersAPI = createAsyncThunk('goods/pathUsersAPI', async (data) => {
-    const res = await axios.patch('http://localhost:3001/users/'+ data.id ,  data.obj)
+    const res = await axios.patch('http://localhost:7777/users/'+ data.id ,  data.obj)
 
     return res.data
 })
